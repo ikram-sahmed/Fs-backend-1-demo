f699d08dfe02f83baef13b44a05007dd62aaab43
Ikram Ahmed <ikramsadiq200@gmail.com>
2022-08-18 01:38:01 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require ('express')
+const app = express();
+const cors = require ('cors')
+
+
+app.use (cors());
+app.use (express.json());
+
+const inventory = ['greeting card', 'wagon', 'computer', 'table', 'chair', 'milk', 'sailboat', 'conditioner', 'rusty nail', 'desk'];
+
+app.get('/api/inventory', (req, res) => {
+    console.log('in the "/api/inventory" endpoint callback')
+    console.log(req.query)
+    res.status(200).send (inventory)
+});
+
+app.get ('/api/inventory/:index', (req, res) => {
+    console.log('in the "/api/inventory/:index" endpoint callback')
+    console.log(req.params.index)
+    const index = Number(req.params.index)
+    res.status(200).send (inventory[index])
+});
+
+
+
+const SERVER_PORT = 5050;
+app.listen (SERVER_PORT, () => console.log(`listening on port ${SERVER_PORT}`))
